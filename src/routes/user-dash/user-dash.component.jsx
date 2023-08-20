6add1a257664c5561a954e86dcc02bfcc057416a
Kumar Kalyan <kum8748ar@gmail.com>
2023-08-20 20:13:25 +0530

    Fix user dash

--- src/routes/user-dash/user-dash.component.jsx
+++ src/routes/user-dash/user-dash.component.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { fetchUserOrders } from "../../utils/firebase/firebase.utils"
 import { UserDashContainer, UserDashNav } from "./user-dash.styles";
 import Button from "../../components/button/button.component";
@@ -6,23 +6,22 @@ import Order from "../../components/order/order.component";
 // import { OrdersContext } from "../../context/order.context";
 import { UserContext } from "../../context/user.context";
 const UserDash = () => {
+
+    const [dashComp, setDashComp] = useState(false)
+    const [profileComp, setProfileComp] = useState(true)
     const { userOrdersMap } = useContext(UserContext)
-
-
-    const handleFetch = async () => {
-    }
 
     return (<UserDashContainer>
         <UserDashNav>  <Button buttonType='inverted'>Profile</Button>
-            <Button buttonType='inverted'>Orders</Button>
-            <Button onClick={handleFetch} >Fetch Order </Button>
+            <Button buttonType='inverted'> Orders</Button>
+            <Button >Fetch Order </Button>
         </UserDashNav>
         {/* Orders  Component */}
         {Object.keys(userOrdersMap).map((obj) => {
             const order = userOrdersMap[obj]
-            const { total, status} = order
+            const { total, status } = order
             return (
-                <Order key={obj} total={total} status={status}  orderId={obj} />
+                <Order key={obj} total={total} status={status} orderId={obj} />
             );
         })}
 
